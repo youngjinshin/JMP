6d5d1ea6e1a529c3f2bc5887d46da40b8173a56b
Youngjin Shin <mail@jindev.me>
2025-05-23 09:42:22 +0900

    first commit

---
+++ en_mac.sh
@@ -0,0 +1,2 @@
+mv "/Applications/JMP 18.app/Contents/Resources/ko.lproj" "/Applications/JMP 18.app/Contents/Resources/ko.lproj.backup"
+cp -r "/Applications/JMP 18.app/Contents/Resources/en.lproj" "/Applications/JMP 18.app/Contents/Resources/ko.lproj"
---
+++ en_win.bat
@@ -0,0 +1,1 @@
+REG ADD "HKCU\Software\JMP\JMP\18.0\Options" /v "LanguagePref" /t REG_SZ /d en /f
